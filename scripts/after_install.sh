e9301c219d13e28c9cd22c16436caceb24eec99a
Venkat07aws <95399832+Venkat07aws@users.noreply.github.com>
2023-09-03 00:56:06 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd /home/ec2-user/server
+cd /home/ubuntu/server
 
 #_Remove_Unused_Code
-rm -rf node_modules
-rm -rf build
+sudo rm -rf node_modules
+sudo rm -rf build
 
 #Install_node_modules_&_Make_React_Build
-npm  install
+sudo apt -y npm  install
 npm run build
